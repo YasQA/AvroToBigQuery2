7b4319b4734ed77658b83123e0c3e6d3e4ee26d3
YasQA <yaroslav.lebid@gmail.com>
2020-11-22 20:58:51 +0200

    Added gcp pubsub subscription and processing files accordingly to received messages

--- src/main/java/com/yaslebid/AvroToBigQuery/AvroToBigQueryApplication.java
+++ src/main/java/com/yaslebid/AvroToBigQuery/AvroToBigQueryApplication.java
@@ -1,10 +1,15 @@
 package com.yaslebid.AvroToBigQuery;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AvroToBigQueryApplication {
+	public static final Logger LOGGER =
+			LoggerFactory.getLogger(AvroToBigQueryApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(AvroToBigQueryApplication.class, args);
 	}
